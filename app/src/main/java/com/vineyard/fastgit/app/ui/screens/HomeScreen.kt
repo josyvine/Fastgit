4078b4cec550c1fb6be793899b5698547092c71d
josyvine <64621103+josyvine@users.noreply.github.com>
2026-08-03 02:00:09 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/vineyard/fastgit/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/vineyard/fastgit/app/ui/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
