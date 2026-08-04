c2b69bfcf7813657b825a796fbb3fb5fe45afa2b
josyvine <64621103+josyvine@users.noreply.github.com>
2026-08-04 22:19:59 +0530

    Update SettingsScreen.kt

--- app/src/main/java/com/vineyard/fastgit/app/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/vineyard/fastgit/app/ui/screens/SettingsScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vineyard.fastgit.app.models.Repository
